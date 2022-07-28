404ac4d2d7f7604d8d20e5ec4fbcd1d0a9aabf6c
Chinmay <csahas922@gmail.com>
2022-07-28 15:38:13 -0400

    Corrected wt fraction calculation error

--- app.py
+++ app.py
@@ -82,7 +82,7 @@ def calculate():
                 summation += (MWi * vhead * Pi) / (R * temp * cmpds[selected]['H0'] * mLnought)
                 print(i)
             print(summation)
-            wtfrac = 1 / (recorded_dict.get(selected) * ((1 / cmpds[selected]['H0']) - summation + (vhead * cmpds[selected]['MW']) / R * temp * mLnought))
+            wtfrac = 1 / (recorded_dict.get(selected) * ((1 / cmpds[selected]['H0']) - summation + (vhead * cmpds[selected]['MW']) / (R * temp * mLnought)))
 
     sorted_dict = dict(sorted(recorded_dict.items()))
 
